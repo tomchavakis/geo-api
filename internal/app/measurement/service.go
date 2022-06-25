84723d8e4af5f2e0ce294b3aab908cc0c26c0955
Thomas Chavakisgit <tomchavakis@gmail.com>
2022-06-25 21:55:09 +0200

    Get Nearest Point Implementation

--- internal/app/measurement/service.go
+++ internal/app/measurement/service.go
@@ -8,4 +8,5 @@ type Service interface {
 	GetBearing(x geometry.Point, y geometry.Point) (*float64, error)
 	GetDestination(x geometry.Point, distance float64, bearing float64, units string) (*geometry.Point, error)
 	GetMidPoint(x geometry.Point, y geometry.Point) geometry.Point
+	GetNearestPoint(refPoint geometry.Point, points []geometry.Point, units string) (*geometry.Point, error)
 }
--- internal/infra/repository/geo/measurement.go
+++ internal/infra/repository/geo/measurement.go
@@ -2,6 +2,7 @@ package measurement
 
 import (
 	"github.com/tomchavakis/geojson/geometry"
+	c "github.com/tomchavakis/turf-go/classification"
 	"github.com/tomchavakis/turf-go/constants"
 	m "github.com/tomchavakis/turf-go/measurement"
 )
@@ -15,7 +16,7 @@ func New() (*Repository, error) {
 	return &Repository{}, nil
 }
 
-// GetDistance returns the distance of two points
+// GetDistance returns the distance of two points.
 func (r *Repository) GetDistance(x, y geometry.Point) (*float64, error) {
 	d, err := m.PointDistance(x, y, constants.UnitMeters)
 	if err != nil {
@@ -25,14 +26,14 @@ func (r *Repository) GetDistance(x, y geometry.Point) (*float64, error) {
 	return &d, nil
 }
 
-// GetBearing returns the bearing of two points
+// GetBearing returns the bearing of two points.
 func (r *Repository) GetBearing(x, y geometry.Point) (*float64, error) {
 	b := m.PointBearing(x, y)
 
 	return &b, nil
 }
 
-// GetDestination returns a point located in a specific destance and bearing from a reference point
+// GetDestination returns a point located in a specific destance and bearing from a reference point.
 func (r *Repository) GetDestination(x geometry.Point, d, b float64, units string) (*geometry.Point, error) {
 	dest, err := m.Destination(x, d, b, units)
 	if err != nil {
@@ -42,6 +43,17 @@ func (r *Repository) GetDestination(x geometry.Point, d, b float64, units string
 	return dest, nil
 }
 
+// GetNearestPoint takes a reference point and a list of points and returns the point from the list closest to the reference point. default units is kilometers
+func (r *Repository) GetNearestPoint(refPoint geometry.Point, points []geometry.Point, units string) (*geometry.Point, error) {
+	res, err := c.NearestPoint(refPoint, points, units)
+
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// GetMidPoint returns the point between two other points.
 func (r *Repository) GetMidPoint(x, y geometry.Point) geometry.Point {
 	mid := m.MidPoint(x, y)
 
